94915de458ba5c06b1eadada555a94e3278c82fa
camilolima1 <caamilolima@gmail.com>
2022-10-22 22:18:36 -0300

    :sparkles: feat: add authorization and authentication to routes

--- src/routes/comorbidity.ts
+++ src/routes/comorbidity.ts
@@ -2,14 +2,16 @@ import {Router} from "express";
 import 'reflect-metadata'
 import { container } from "tsyringe";
 import ComorbidityController from "../controller/ComorbidityController";
+import EnsureAdmin from "../middlewares/EnsureAdmin";
+import EnsureAuthenticated from "../middlewares/EnsureAuthenticated";
 
 const comorbidityRoutes = Router();
 
 const comorbidityController = container.resolve<ComorbidityController>(ComorbidityController)
 
-comorbidityRoutes.post('/', (req, res) => comorbidityController.create(req,res));
-comorbidityRoutes.get('/', (req, res) => comorbidityController.findAll(req,res));
-comorbidityRoutes.put('/:id', (req, res) => comorbidityController.update(req,res));
-comorbidityRoutes.delete('/:id', (req, res) => comorbidityController.delete(req,res));
+comorbidityRoutes.post('/', EnsureAuthenticated, (req, res) => comorbidityController.create(req,res));
+comorbidityRoutes.get('/', EnsureAuthenticated, (req, res) => comorbidityController.findAll(req,res));
+comorbidityRoutes.put('/:id', EnsureAuthenticated, EnsureAdmin, (req, res) => comorbidityController.update(req,res));
+comorbidityRoutes.delete('/:id', EnsureAuthenticated, EnsureAdmin, (req, res) => comorbidityController.delete(req,res));
 
 export {comorbidityRoutes};
